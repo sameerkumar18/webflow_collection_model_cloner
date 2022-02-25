c2e45027cdd91aa12541ea3b53c9e164267bbe1f
Sameer Kumar <sameer18051998@gmail.com>
2022-02-25 11:09:10 +0000

    fixed get_json()

--- app.py
+++ app.py
@@ -12,30 +12,32 @@ from lxml import html
 app = flask.Flask(__name__)
 CORS(app)
 
+
 @app.route('/v1/clone_schema', methods=["post"])
 def main():
-  input_data = request.get_data()
-  wf_clone = WebflowCollectionSchema(
-      origin_config=input_data['origin'],
-      destination_config=input_data['destination'])
-  try:
-      collection_dict = wf_clone.get_collection_schema(
-          input_data['origin']['collection_id'])
-      updated_collection_dict = wf_clone.check_and_update_refrences(
-          collection_dict)
-      # print('collection for req: ', updated_collection_dict)
+    input_data = request.get_json()
+    print(input_data, type(input_data))
+    wf_clone = WebflowCollectionSchema(
+        origin_config=input_data['origin'],
+        destination_config=input_data['destination'])
+    try:
+        collection_dict = wf_clone.get_collection_schema(
+            input_data['origin']['collection_id'])
+        updated_collection_dict = wf_clone.check_and_update_refrences(
+            collection_dict)
+        # print('collection for req: ', updated_collection_dict)
 
-      print('==============')
-      new_collection_dict = wf_clone.create_collection(updated_collection_dict)
-      print('new collection for req: ', new_collection_dict)
+        print('==============')
+        new_collection_dict = wf_clone.create_collection(
+            updated_collection_dict)
+        print('new collection for req: ', new_collection_dict)
 
-      print('==============')
-      return jsonify(new_collection_dict)
-  except requests.exceptions.HTTPError as httperr:
-      print(httperr)
-      print(httperr.response.text)
-      return jsonify(httperr.response.json()), httperr.response.status_code
-
+        print('==============')
+        return jsonify(new_collection_dict)
+    except requests.exceptions.HTTPError as httperr:
+        print(httperr)
+        print(httperr.response.text)
+        return jsonify(httperr.response.json()), httperr.response.status_code
 
 
 if __name__ == "__main__":
